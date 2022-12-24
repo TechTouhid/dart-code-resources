aef8735b3927dc98ba775f56120d80c5ef06ea0c
TechTouhid <touhidalamin123@gmail.com>
2022-12-25 03:15:17 +0600

    Created a Dart Project about variable and data type

---
+++ variable_data_type/bin/variable_data_type.dart
@@ -0,0 +1,5 @@
+// fswatch -o bin/ | xargs -n1 -I{} sh -c 'clear; dart bin/variable_data_type.dart'
+
+void main(List<String> args) {
+  print('hello world this is a data file');
+}
---
+++ variable_data_type/test/variable_data_type_test.dart
@@ -0,0 +1,8 @@
+import 'package:variable_data_type/variable_data_type.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
